2167f6bba30d0a284847dae44fa6ee0e15525277
Daniel Millikan <dmillikan@gmail.com>
2021-02-09 23:56:20 -0600

    go go gadget

--- promote.sh
+++ promote.sh
@@ -17,31 +17,33 @@ trigger() {
 
 response=$(
     curl -X POST \
-    --verbose \
     --header "Circle-Token: $5" \
     --header "Content-Type: application/json" \
     --header 'Accept: application/json' \
     --data '{
         "parameters": {
-        "environment": "'$1'"
+        "environment": "'"$1"'"
         }
     }' \
-    https://circleci.com/api/v2/project/$2/$3/$4/pipeline
+    https://circleci.com/api/v2/project/"$2"/"$3"/"$4"/pipeline
 )
 
-echo $response
+echo "$response"
 }
 
 set_defaults(){
-    export ENVIRONMENT="dev"
+    export ENVIRONMENT="dm"
     export REPO_CODE="gh"
     export CIRCLE_PROJECT_USERNAME="dmillikan"
     export CIRCLE_PROJECT_REPONAME="circle-lab"
     
 }
-
-set_defaults
+if [ ! "$CI" ]
+then
+    echo "Setting defaults"
+    set_defaults
+fi
 echo "Environment before promotion is   :" $ENVIRONMENT
 promote
 echo "Environment after promotion is    :" $ENVIRONMENT
-# trigger $ENVIRONMENT $REPO_CODE $CIRCLE_PROJECT_USERNAME $CIRCLE_PROJECT_REPONAME $CIRCLE_TOKEN
+trigger "$ENVIRONMENT" "$REPO_CODE" "$CIRCLE_PROJECT_USERNAME" "$CIRCLE_PROJECT_REPONAME" "$CIRCLE_TOKEN"
