8e5397b2adc09fee240aa6a9ac2eb86079083825
Daniel Millikan <dmillikan@gmail.com>
2021-02-10 00:15:30 -0600

    update script

--- promote.sh
+++ promote.sh
@@ -1,38 +1,38 @@
 #! /bin/bash
 
-promote() {
+promote(){
     case $ENVIRONMENT in
-        "dev")
+        dev)
             ENVIRONMENT="preprod"
         ;;
-        "preprod")
+        preprod)
             ENVIRONMENT="prod"
+        ;;
+        *)
+            ENVIRONMENT="terminate"
         ;;
     esac
     export ENVIRONMENT
 }
 
-
-trigger() {
-
-response=$(
-    curl -X POST \
-    --header "Circle-Token: $5" \
-    --header "Content-Type: application/json" \
-    --header 'Accept: application/json' \
-    --data '{
-        "parameters": {
-        "environment": "'"$1"'"
-        }
-    }' \
-    https://circleci.com/api/v2/project/"$2"/"$3"/"$4"/pipeline
-)
-
-echo "$response"
+trigger(){
+    response=$(
+        curl -X POST \
+        --header "Circle-Token: $5" \
+        --header "Content-Type: application/json" \
+        --header 'Accept: application/json' \
+        --data '{
+            "parameters": {
+            "environment": "'"$1"'"
+            }
+        }' \
+        https://circleci.com/api/v2/project/"$2"/"$3"/"$4"/pipeline
+    )
+    echo "$response"
 }
 
 set_defaults(){
-    export ENVIRONMENT="dm"
+    export ENVIRONMENT="dev"
     export REPO_CODE="gh"
     export CIRCLE_PROJECT_USERNAME="dmillikan"
     export CIRCLE_PROJECT_REPONAME="circle-lab"
@@ -47,9 +47,13 @@ else
     set_defaults
 fi
 
-echo "Environment before promotion is   :" $ENVIRONMENT
+echo "Environment before promotion is   :" "$ENVIRONMENT"
 promote
-echo "Environment after promotion is    :" $ENVIRONMENT
-export > a
-cat a
-trigger "$ENVIRONMENT" "$REPO_CODE" "$CIRCLE_PROJECT_USERNAME" "$CIRCLE_PROJECT_REPONAME" "$CIRCLE_TOKEN"
+echo "Environment after promotion is    :" "$ENVIRONMENT"
+# export > a
+# cat a
+if [ "$ENVIRONMENT" != "terminate" ]
+then
+    echo "Will trigger workflow for" "$ENVIRONMENT"
+    #trigger "$ENVIRONMENT" "$REPO_CODE" "$CIRCLE_PROJECT_USERNAME" "$CIRCLE_PROJECT_REPONAME" "$CIRCLE_TOKEN"
+fi
